c40b484abe8561f6e33f4868052c8c7b47bf3a24
Avery Bai <simple.oriented@gmail.com>
2022-06-26 08:18:36 +0800

    fix build

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on the feature branch.
+          learn react I was changed on the feature branch.
         </a>
       </header>
     </div>
